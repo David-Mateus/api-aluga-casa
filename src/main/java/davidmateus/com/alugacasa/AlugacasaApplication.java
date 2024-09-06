b341d09e5cd3f12a941700a003c8c261aa13f64e
David-Mateus <david-mateus09@hotmail.com>
2024-09-06 18:44:09 -0300

    feat: first commit

---
+++ src/main/java/davidmateus/com/alugacasa/AlugacasaApplication.java
@@ -0,0 +1,13 @@
+package davidmateus.com.alugacasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugacasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugacasaApplication.class, args);
+	}
+
+}
