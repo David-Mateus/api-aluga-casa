98d8b148eef83854a9cff11cb5527f06f9a2a11c
David-Mateus <david-mateus09@hotmail.com>
2024-10-29 20:25:34 -0300

    refactor: add method findByUserName

--- src/main/java/davidmateus/com/alugacasa/repository/UserRepository.java
+++ src/main/java/davidmateus/com/alugacasa/repository/UserRepository.java
@@ -4,10 +4,13 @@ import davidmateus.com.alugacasa.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository  extends JpaRepository<User, Long> {
 
     // Usando JPQL(Java persistence query Language) User ai na query faz referencia a entidade.JPQL opera com base em entidades
     // @Query("SELECT u.username, u.id FROM User u")
     // List<String> findAllNames();
+    Optional<User> findByUserName(String username);
 }
