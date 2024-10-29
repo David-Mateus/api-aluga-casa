250ce0e4a0e375a980763615109e400a45dfe1ba
David-Mateus <david-mateus09@hotmail.com>
2024-10-29 20:27:57 -0300

    feat: implements interfaces token

---
+++ src/main/java/davidmateus/com/alugacasa/userdetails/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package davidmateus.com.alugacasa.userdetails;
+
+import davidmateus.com.alugacasa.model.User;
+import davidmateus.com.alugacasa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username).orElseThrow(() -> new RuntimeException("Usuario não encontrado;"));
+        return new UserDetailsImpl(user);
+
+    }
+}
